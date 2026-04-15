cb40a9a5b761943fecd3b1dab41020bab7af654d
KloudKid <43523402+KloudKid@users.noreply.github.com>
2026-04-14 19:27:17 -0500

    Simulación de envío de datos en tiempo real con Kafka.

--- producer.py
+++ producer.py
@@ -1,5 +1,5 @@
 # Simulación de datos en tiempo real
-
+# Importamos librerías necesarias
 import time
 import json
 from kafka import KafkaProducer
@@ -9,11 +9,14 @@ producer = KafkaProducer(
     bootstrap_servers='localhost:9092',
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
-
+# Abrimos el archivo CSV que contiene los datos
 with open('siembra_palmira.csv', encoding='utf-8', errors='ignore') as file:
     reader = csv.DictReader(file)
-
+# Recorrer cada fila del archivo
     for row in reader:
+        # Enviamos cada fila al topic de Kafka
         producer.send('Siembras_Palmira', row)
+        # Mostramos en consola lo que se está enviando
         print("Enviado:", row)
+        # Esperamos 1 segundo antes de enviar el siguiente dato
         time.sleep(1)
